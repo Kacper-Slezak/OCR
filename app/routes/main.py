23902666dc424750c078b0141927f4f160c56c6a
Nix <tym.wozniak@gmail.com>
2025-06-20 15:47:09 +0200

    dashboard working, cant check recpit tho

--- app/routes/main.py
+++ app/routes/main.py
@@ -4,15 +4,14 @@ from flask_login import current_user, login_required
 
 from app import db
 from app.models import ShoppingList
-from app.models import Receipt
 
 bp = Blueprint('main', __name__)
 
 @bp.route('/', methods=['GET'])
 @login_required
-def home():
-    user_recipts = ShoppingList.query.filter_by(owner_id=current_user.id).all()
-    return render_template('app/dashboard.html', recipts=user_recipts)
+def dashboard():
+    user_recipts = ShoppingList.query.filter_by(created_by=current_user.id).all()
+    return render_template('main/dashboard.html', recipts=user_recipts)
 
 # Możesz tu dodać inne trasy, np. dla statycznych stron "O nas", "Kontakt", jeśli kiedyś je zrobisz
 # @bp.route('/about')
--- app/routes/recipt.py
+++ app/routes/recipt.py
@@ -1,14 +1,13 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash
 from flask_login import login_required, current_user
 from app import db
-from app.models import ShoppingList
 from app.models import Receipt
 
-recipt_bp = Blueprint('shopping', __name__, url_prefix='/recipt')
+recipt_bp = Blueprint('recipt', __name__, url_prefix='/recipt')
 
 @recipt_bp.route('/<int:recipt_id>', methods=['GET'])
 @login_required
 def recipt(receipt_id):
     current_receipt = Receipt.query.filter_by(id=receipt_id, user_id=current_user.id).first_or_404()
     parsed_data = current_receipt.get_processed_data()
-    return render_template('app/recipt.html', receipt=current_receipt, parsed_data=parsed_data)
+    return render_template('/recipt/recipt.html', receipt=current_receipt, parsed_data=parsed_data)
