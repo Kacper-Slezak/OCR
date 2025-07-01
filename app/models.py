c4a1cc57be2c13d0794da340b68e1e0203c6d2f0
Kacper-Slezak <422843@student.agh.edu.pl>
2025-07-01 11:41:08 +0200

    Changes in settlements.py and settlements_services.py and main.py

--- app/models.py
+++ app/models.py
@@ -72,7 +72,7 @@ class User(db.Model, UserMixin):
 class ShoppingList(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
-    created_by = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    created_by = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)g
     created_at = db.Column(db.DateTime, default=datetime.now)
     is_completed = db.Column(db.Boolean, default=False)
     # NOWA KOLUMNA: Status rozliczenia całej listy
